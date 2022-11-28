fb7f71272ac6ebe69c1fe2bbdc4439bbd712ed04
Dzenis Zigo <dzeniszigo@gmail.com>
2022-11-27 20:32:27 -0500

    Got the css fixed, a few wrong things were happening but everything appears to look how it's supposed to now

--- SalonML/SalonML/src/app/core/resume-header/resume-header.component.ts
+++ SalonML/SalonML/src/app/core/resume-header/resume-header.component.ts
@@ -3,15 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-resume-header',
   templateUrl: './resume-header.component.html',
-  styleUrls: ['./resume-header.component.css',
-    '../../shared/vendor/css/bundle.min.css',
-    '../../shared/vendor/css/LineIcons.min.css',
-    '../../shared/vendor/css/revolution-settings.min.css',
-    '../../shared/vendor/css/jquery.fancybox.min.css',
-    '../../shared/vendor/css/owl.carousel.min.css',
-    '../../shared/vendor/css/cubeportfolio.min.css',
-    '../../shared/resume/css/style.css'
-  ]
+  styleUrls: []
 })
 export class ResumeHeaderComponent implements OnInit {
 
