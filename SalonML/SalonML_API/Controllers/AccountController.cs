5a836b4407659a9e1a690c241a3f0811ba86977c
Dzenis Zigo <dzeniszigo@gmail.com>
2022-11-30 17:44:50 -0500

    Finish login functionality (back end)

--- SalonML/SalonML_API/Controllers/AccountController.cs
+++ SalonML/SalonML_API/Controllers/AccountController.cs
@@ -1,22 +1,90 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using SalonML_API.Data;
 using SalonML_API.Data.Models;
-using System.Net;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SalonML_API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
-        //private readonly JwtHandler _jwtHandler;
+        private readonly IConfiguration _config;
 
-        [HttpPost("Login")]
+        public AccountController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            IConfiguration configuration)
+        {
+            _context = context;
+            _userManager = userManager;
+            _config = configuration;
+        }
+
+        [HttpPost]
         public async Task<IActionResult> Login(LoginRequest loginRequest)
         {
-            return Ok();
+            // verify email & password
+            var user = await _userManager.FindByNameAsync(loginRequest.Email);
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, loginRequest.Password))
+                return Unauthorized(new LoginResult()
+                {
+                    Success = false
+                });
+
+            // create and return a token
+            var secToken = await CreateTokenAsync(user);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(secToken);
+
+            return Ok(new LoginResult()
+            {
+                Success = true,
+                Token = jwt
+            });
+        }
+
+        private async Task<JwtSecurityToken> CreateTokenAsync(ApplicationUser user)
+        {
+            var jwtOptions = new JwtSecurityToken(
+                issuer: _config["JwtSettings:Issuer"], //TODO verify iss and aud was done correctly
+                audience: _config["JwtSettings:Audience"],
+                claims: await GetClaimsAsync(user),
+                expires: DateTime.Now.AddHours(
+                    Convert.ToInt16(_config["JwtSettings:ExpirationTimeInHours"])),
+                signingCredentials: GetSigningCredentials()
+            );
+
+            return jwtOptions;
+        }
+
+        private async Task<List<Claim>> GetClaimsAsync(ApplicationUser user)
+        {
+            var claims = new List<Claim>();
+
+            // todo check if Name: <Email> is required claim
+
+            foreach (var role in await _userManager.GetRolesAsync(user))
+                claims.Add(new Claim(ClaimTypes.Role, role));
+
+            return claims;
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _config["JwtSettings:SecurityKey"]));
+
+            return new SigningCredentials(
+                key, 
+                SecurityAlgorithms.HmacSha256);
         }
     }
 }
---
+++ SalonML/SalonML_API/Data/LoginResult.cs
@@ -0,0 +1,15 @@
+﻿namespace SalonML_API.Data
+{
+    public class LoginResult
+    {
+        /// <summary>
+        /// TRUE if the login attempt is successful, FALSE otherwise.
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// The JWT token if the login attempt is successful, or NULL if not
+        /// </summary>
+        public string? Token { get; set; }
+    }
+}
