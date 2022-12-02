40816836555a5ad5f2c492fe0b7796cc158b2942
Dzenis Zigo <dzeniszigo@gmail.com>
2022-12-01 19:31:36 -0500

    setup routing for new-password, as well as point template path to admin-form and stylesheet to the resume shared css

--- SalonML/SalonML/src/app/core/admin-login/new-password/new-password.component.ts
+++ SalonML/SalonML/src/app/core/admin-login/new-password/new-password.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-password',
-  templateUrl: './new-password.component.html',
-  styleUrls: ['./new-password.component.css']
+  templateUrl: '../admin-form.html',
+    styleUrls: ['./new-password.component.css',
+        '../../../shared/resume/css/style.css']
 })
 export class NewPasswordComponent implements OnInit {
 
--- SalonML/SalonML/src/app/core/app-routing.module.ts
+++ SalonML/SalonML/src/app/core/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ForgotPasswordComponent } from './admin-login/forgot-password/forgot-password.component';
 import { LoginComponent } from './admin-login/login/login.component';
+import { NewPasswordComponent } from './admin-login/new-password/new-password.component';
 import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'forgot-password', component: ForgotPasswordComponent }
+    { path: 'new-password/:token', component: NewPasswordComponent }
 ];
 
 @NgModule({
