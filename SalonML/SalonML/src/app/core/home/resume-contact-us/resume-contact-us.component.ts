984f91d384abf975ce2de369f99af58e5ad6320f
Dzenis Zigo <dzeniszigo@gmail.com>
2022-12-02 19:37:14 -0500

    fix phone element in the contact info section

--- SalonML/SalonML/src/app/core/home/resume-contact-us/resume-contact-us.component.ts
+++ SalonML/SalonML/src/app/core/home/resume-contact-us/resume-contact-us.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-resume-contact-us',
   templateUrl: './resume-contact-us.component.html',
-  styleUrls: ['./resume-contact-us.component.css',
-    '../../../shared/resume/css/style.css'],
+  styleUrls: [
+    '../../../shared/resume/css/style.css',
+    './resume-contact-us.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class ResumeContactUsComponent implements OnInit {
