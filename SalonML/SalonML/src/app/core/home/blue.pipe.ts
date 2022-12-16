bfc6ddb8ce376f4f3be28369cffe776b7a6dda99
Dzenis Zigo <dzeniszigo@gmail.com>
2022-12-15 20:21:59 -0500

    improve pipe to hide empty values to guests and display a dot for admin users

--- SalonML/SalonML/src/app/core/home/blue.pipe.ts
+++ SalonML/SalonML/src/app/core/home/blue.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 /*
  * Raise the value exponentially
  * Takes an exponent argument that defaults to 1.
@@ -10,9 +11,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 */
 @Pipe({ name: 'blue' })
 export class BluePipe implements PipeTransform {
+  isAdmin: boolean;
+
+  constructor(private authService: AuthService) {
+    this.isAdmin = authService.isLoggedIn;
+  }
+
   transform(value: string): string {
     if (value == null)
       return value;
+
+    // hide deleted values from ordinary visitors/guests
+    if (value === "." && !this.isAdmin)
+      return "";
 
     value = value.replace(/{/gi, "<span class=\"main-color\">");
     value = value.replace(/}/gi, "</span>");
