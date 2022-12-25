49a2307e21491389fce57de858ac8328e50422ba
Dzenis Zigo <dzeniszigo@gmail.com>
2022-12-25 12:25:03 -0500

    remove ununsed code

--- SalonML/SalonML/src/app/core/home/home/home.component.ts
+++ SalonML/SalonML/src/app/core/home/home/home.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { environment } from '../../../../environments/environment';
 import { AuthService } from '../../auth/auth.service';
 import { BaseHomeComponent } from '../base-home.component';
 import { DynamicContentService } from '../dynamic-content.service';
@@ -12,34 +10,22 @@ import { DynamicContentService } from '../dynamic-content.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent extends BaseHomeComponent implements OnInit {
-  localization = "en";
-
   constructor(private router: Router,
               protected override authService: AuthService,
               protected override dynContentService: DynamicContentService) {
     super(authService, dynContentService);
 
     if (this.router.url === "/admin-test") {
+      console.log("Logging in test admin");
+
       authService.loginTestAdmin();
 
       this.router.navigate(["/"]).then(() => {
         window.location.reload();
       });
     }
-
-    dynContentService.onNewDataLoaded.subscribe((l10n: string) => {
-      this.localization = l10n;
-    });
   }
 
   ngOnInit(): void {
-  }
-
-  isEnglishLocalization() {
-    return this.localization === "en";
-  }
-
-  isBosnianLocalization() {
-    return this.localization === "bih";
   }
 }
